69f26b05f366e737caec379e3ea2d545884dacc8
Jeffrey <jdsun010@gmail.com>
2023-09-16 16:19:45 -0700

    add meter modal component

---
+++ src/components/AddMeterModal.jsx
@@ -0,0 +1,36 @@
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+
+import MeterForm from "./MeterForm";
+
+const style = {
+  position: "absolute",
+  top: "40%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "40%",
+  bgcolor: "background.paper",
+  border: "1px solid #000",
+  boxShadow: 12,
+  borderRadius: 2,
+  p: 4,
+  color: "black",
+};
+
+const AddMeterModal = ({ showModal, setShowModal, setTable }) => {
+  // create POST request handler here. on success, close modal. pass handler to MeterForm.
+  return (
+    <Modal
+      open={showModal}
+      onClose={() => setShowModal(false)}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <MeterForm title="Create a Meter" setShowModal={setShowModal} setTable={setTable} />
+      </Box>
+    </Modal>
+  );
+};
+
+export default AddMeterModal;
